bd7a67c210b054f9d79fbbe7fcd5bef9ac86d473
Eru3726 <suraimu20040127@gmail.com>
2023-06-20 11:31:59 +0900

    EnemyGeneの追加

--- SouceTreeShooting/Assets/Scripts/EnemyGene/EnemyGene.cs
+++ SouceTreeShooting/Assets/Scripts/EnemyGene/EnemyGene.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class EnemyGene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject enemyObj;
+    public int createTime;
+
+    private float timer;
+
+    private Vector2 pos;
+
+    private void Start()
     {
-        
+        pos = new Vector2(-2.75f, 3.0f);
+        Instantiate(enemyObj, pos, Quaternion.identity);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer >= createTime)
+        {
+            timer = 0f;
+            pos = new Vector2(Random.Range(-6f, 0.5f), Random.Range(2f, 4f));
+            Instantiate(enemyObj, pos, Quaternion.identity);
+        }
     }
 }
