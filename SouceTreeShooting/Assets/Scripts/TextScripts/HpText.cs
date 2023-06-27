e58d6f93be28d1907850712377762e5438438597
DESKTOP-67MT2K8\user <showjapan@icloud.com>
2023-06-27 14:59:40 +0900

    HPのUI関連の変換。スクリプトの中身は変化なし。　by show

---
+++ SouceTreeShooting/Assets/Scripts/TextScripts/HpText.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpText : MonoBehaviour
+{
+    // Start is called before the firs
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       
+    }
+}
